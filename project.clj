9e990bac69796e8f03fa1357010d6444e6021f78
Edwin Watkeys <edw@poseur.com>
2013-12-16 15:32:46 -0500

    words

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject edw/loggle "0.1.0"
-  :description "Bargain-basement logging handler"
+  :description "Bargain-basement logging"
   :url "http://github.com/edw/loggle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
